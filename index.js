9a8d3b52faedaf7fd0be05dba02c11a1bcdd38f7
Annes khan <109712725+anees-web@users.noreply.github.com>
2022-08-30 05:01:28 -0700

    Uploaded html and JS files

---
+++ index.js
@@ -0,0 +1,16 @@
+function max(arr){
+    
+    let m=arr[0];//2 -> 3 -> 4 -> 5
+    for(let i=1; i<arr.length; i++){
+
+        if(arr[i]>m){
+        //in first loop we compared 2 with arr[1]=1; 2
+        //2nd loop, comapring 3>2
+        //3rd loop, comapring 4>3
+        m=arr[i];
+    }
+    return pos;
+    
+}
+let marks=[22,91,53,46,52,82]
+max(arr);
